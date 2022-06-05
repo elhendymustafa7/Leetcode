d86de497ea4484c348f7c8fbcaf92328f6f9a76a
Mustafa Elhendy <58703269+elhendymustafa7@users.noreply.github.com>
2022-06-06 01:07:33 +0200

    Time: 192 ms (47.16%), Space: 45.4 MB (73.26%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+         var ans = new int[nums.Length*2];
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[i];
+                ans[i+nums.Length] = nums[i];
+                
+            }
+            return ans;
+    }
+}
